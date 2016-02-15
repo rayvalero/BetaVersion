8c1412e184bbd4702a0ad78ddfac77aa5b1e6b8c
RayValero <rayvalero177@gmail.com>
2016-02-15 03:36:50 -0430

    0028.pl

---
+++ 0028.pl
@@ -0,0 +1,200 @@
+% 0028.pl  version alfa 0.1 2015-06-16 
+% This program is free software 
+% Teran Valero A. Ray Enmanuel
+% 21.182.424
+% rayvalero@gmail.com
+% Matematicas Discretas 
+% Universidad de los Andes
+
+
+% definiciomes
+
+pr(0028-, [Numeros, concepto , general], ['Un número, en ciencia, es una abstracción que representa una cantidad o una magnitud'], 20/20, ['Ray Valero']).
+
+pr(0028-, [NumeroReal, concepto , general], ['En matemáticas, el conjunto de los números reales (denotado por ℝ) incluye tanto a los números racionales (positivos, negativos y el cero) como a los números irracionales'], 20/20, ['Ray Valero']).
+
+pr(0028-, [NumeroNatural, concepto , general], ['Un número natural es cualquiera de los números que se usan para contar los elementos de un conjunto.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [NumeroRacional, concepto , general], ['es todo número que puede representarse como el cociente de dos números enteros o, más precisamente, un entero y un natural positivo'], 20/20, ['Ray Valero']).
+
+pr(0028-, [NumeroEntero, concepto , general], ['Los números enteros son elementos de un conjunto de números que reúne a los positivos (1, 2, 3, ...), a los negativos opuestos de los anteriores: (..., −3, −2, −1) y al 0'], 20/20, ['Ray Valero']).
+
+pr(0028-, [MatematicaDiscreta, concepto , general], ['es un Area de la matematica encargada del estudio de los conjuntos discretos finitos o infinitos numerables.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [PROLOG, definicion, general], ['Es un lenguaje de programacion logica'], 20/20, ['Ray Valero']).
+
+pr(0028-, [semantica, concepto , general], ['Parte de la lingüística que estudia el significado de las expresiones lingüísticas'], 20/20, ['Ray Valero']).
+
+pr(0028-, [algoritmo, concepto , general], ['Un algoritmo es un conjunto finito de instrucciones precisas que resuelven un problema o realizan un cálculo'], 20/20, ['Ray Valero']).
+
+pr(0028-, [proposición, definición, general], ['En lógica, el término proposición se usa para referirse a las entidades portadoras de los valores de verdad.'],20/20, ['Ray Valero']).
+
+pr(0028-, [argumento, definición, general], ['Razonamiento que demuestra, refuta o justifica algo.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [ValidezDeArgumento, definición, general], ['Un argumento concreto es válido cuando tiene la forma de un esquema de argumento válido.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [complejidad, concepto , general], ['es la cualidad de lo que está compuesto de diversos elementos interrelacionados'], 20/20, ['Ray Valero']).
+
+pr(0028-, [lógica, definición, general], ['es un idioma artificial que ha sido diseñado de tal forma que sus estructuras gramaticales no sean intrínsecamente ambiguas'], 20/20, ['Ray Valero']).
+
+pr(0028-, [lógica, padre, fundador], ['Aristóteles'], 20/20, ['Ray Valero']).
+
+pr(0028-, [ModusTollendoPonens, concepto , general], ['Es la forma en que al afirmar se niega, Ejemplo: Cora y Nick no pueden ganar ambos la carrera. Ana ganó la carrera.Por lo tanto, Bill no puede haber ganado la carrera'], 20/20, ['Ray Valero']).
+
+pr(0028-, ['¿ Para que sirve la logica ?'],['Para aprender a construir mensajes claros y precisos]', 20/20, ['Ray Valero']).
+
+pr(0028-, [ValidezLogica, definición, general], ['En lógica, la validez es una propiedad que tienen los argumentos cuando las premisas implican la conclusión.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [numero, variables, proposicion, atomica], [una], 20/20, ['Ray Valero']).
+
+pr(0028-, [proposicionMolecular, compuestaPor], ['conexiones de proposiciones atomicas'], 20/20, ['Ray Valero']).
+
+pr(0028-, [tablaDeVerdad, concepto , general], ['es una tabla que muestra el valor de una proposición compuesta, para cada posible combinación de verdad que se pueda asignar'], 20/20, ['Ray Valero']).
+
+pr(0028-, [tautologia, definiciónLogica, general], ['es una fórmula bien formada de un sistema de lógica proposicional que resulta verdadera para cualquier interpretación'], 20/20, ['Ray Valero']).
+
+pr(0028-, [conjunto , conceoto, general], ['Que se hace simultáneamente a otra cosa o con un fin común.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [conjunto , definicionMatematica, general], ['En matemáticas, un conjunto es una colección de elementos considerada en sí misma como un objeto.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [conjuntoVacio, definicion, general], ['El conjunto que no contiene ningún elemento se llama el conjunto vacío y se denota por ∅ o simplemente {}. Existe un único conjunto vacío, ya que lo único que distingue a un conjunto son sus elementos.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [ConjuntoFinito, concepto , general], ['En matemática, un conjunto finito es un conjunto que tiene un número finito de elementos'], 20/20, ['Ray Valero']).
+
+pr(0028-, [ConjuntoInfinito, concepto , general], ['En teoría de conjuntos, un conjunto infinito es un conjunto que no es finito'], 20/20, ['Ray Valero']).
+
+pr(0028-, [subconjunto, definicion, general], ['Un subconjunto A de un conjunto B, es un conjunto que contiene algunos de los elementos de B (o quizá todos)'], 20/20, ['Ray Valero']).
+
+pr(0028-, [OperacionesConConjuntos , concepto , general], ['son Union , Interseccion , Diferencia , Complemento , Diferencia simetrica , Producto cartesiano'],20/20, ['Ray Valero']).
+
+pr(0028-, [Union , concepto , general], ['(símbolo ∪) La unión de dos conjuntos A y B, que se representa como A ∪ B, es el conjunto de todos los elementos que pertenecen al menos a uno de los conjuntos A y B.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [Interseccion , concepto , general], ['(símbolo ∩) La intersección de dos conjuntos A y B es el conjunto A ∩ B de los elementos comunes a A y B.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [Diferencia , concepto , general], ['(símbolo \) La diferencia del conjunto A con B es el conjunto A \ B que resulta de eliminar de A cualquier elemento que esté en B.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [Complemento , concepto , general], ['El complemento de un conjunto A es el conjunto A∁ que contiene todos los elementos que no pertenecen a A, respecto a un conjunto U que lo contiene.'], 20/20, ['Ray Valero']).
+ 
+pr(0028-, [DiferenciaSimetrica , concepto , general], ['(símbolo Δ) La diferencia simétrica de dos conjuntos A y B es el conjunto A Δ B con todos los elementos que pertenecen, o bien a A, o bien a B, pero no a ambos a la vez.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [ProductoCartesiano , concepto , general], ['(símbolo ×) El producto cartesiano de dos conjuntos A y B es el conjunto A × B de todos los pares ordenados (a, b) formados con un primer elemento a perteneciente a A, y un segundo elemento b perteneciente a B.'], 20/20, ['Ray Valero']).
+
+pr(0028-, [DiagramaDeVenn, concepto , general],['Es una representación gráfica, normalmente son líneas circulares u ovoides cerradas que nos muestran las relaciones existentes entre los conjuntos'], 20/20, ['Ray Valero']).
+
+pr(0028-, [funcion, concepto , general], ['Es una relacion binaria porque asocia a cada argumento con un resultado unico llamado la imagen de una funcion'], 20/20, ['Ray Valero']).
+
+pr(0028-, [rango, concepto , general], ['Es el conjunto de todos los puntos para  los cuales existe una preimagen que satisfacey=f(x)'], 20/20, ['Ray Valero']).
+
+pr(0028-, [funcionInyectiva, concepto , general], ['Es una funcion que se relaciona uno a uno los elementos del conjunto de partida con los de llegada'], 20/20, ['Ray Valero']).
+
+pr(0028-, [funcionSobreyectiva, concepto , general], ['Es una funcion en la cual todo los  elementos del conjunto de llegada tiene una preimagen'], 20/20, ['Ray Valero']).
+
+pr(0028-, [funcionBiyectiva, concepto , general], ['Es una funcion que es inyectiva y sobreyectiva al mismo tiempo'], 20/20, ['Ray Valero']).
+
+pr(0028-, [funcionInversa, concepto , general], ['si F es una funcion que  al ser evaluada en una funcion g da 1 y si g al evaluarla en F da 1 esa son funciones inversas una de la otra'], 20/20, ['Ray Valero']).
+
+
+% Tablas de la verdad:
+% Variable
+
+pr(0028-, ['p = V','p','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F','p','='], ['F'], 20/20, ['Ray Valero']).
+
+% Negacion 
+
+pr(0028-, ['p = V', '~p', '='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', '~p', '='], ['V'], 20/20, ['Ray Valero']).
+
+% bidimencionales (p yq ):
+% conjunciones ('p ^ q')
+
+pr(0028-, ['p = V', and, 'q = V', 'p ^ q', '='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'q = F', 'p ^ q', '='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'q = V', 'p ^ q', '='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'q = F', 'p ^ q', '='], ['F'], 20/20, ['Ray Valero']).
+
+% disyunciones ('p v q')
+
+pr(0028-, ['p = V', or, 'q = V', 'p v q', '='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', or, 'q = F', 'p v q', '='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', or, 'q = V', 'p v q', '='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', or, 'q = F', 'p v q', '='], ['F'], 20/20, ['Ray Valero']).
+
+% Implicacion o condicional ('p => q')
+
+pr(0028-, ['p = V', =>, 'q = V', 'p => q', '='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', =>, 'q = F', 'p => q', '='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', =>, 'q = V', 'p => q', '='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', =>, 'q = F', 'p => q', '='], ['V'], 20/20, ['Ray Valero']).
+
+% Equivalencia, doble implicación o Bicondicional ('p <=> q')
+
+pr(0028-, ['p = V', <=>, 'q = V', 'p <=> q', '='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', <=>, 'q = F', 'p <=> q', '='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', <=>, 'q = V', 'p <=> q', '='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', <=>, 'q = F', 'p <=> q', '='], ['V'], 20/20, ['Ray Valero']).
+
+% contradiccion ('p ^ ~q')
+
+pr(0028-, ['p = V', and no, 'q = V', 'p ^ ~q', '='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and no, 'q = F', 'p ^ ~q', '='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and no, 'q = V', 'p ^ ~q', '='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and no, 'q = F', 'p ^ ~q', '='], ['V'], 20/20, ['Ray Valero']).
+
+% Tautologias ('p v ~q')
+
+pr(0028-, ['p = V', or no, 'q = V', 'p v ~q', '='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', or no, 'q = F', 'p v ~q', '='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', or no, 'q = V', 'p v ~q', '='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', or no, 'q = F', 'p v ~q', '='], ['V'], 20/20, ['Ray Valero']).
+
+% tabla de verdad para una proposición compuesta formada a partir de 3 Proposiciones primitivas o simples ( p , q y r):
+% Verdad Indeterminada o Contingencia 'p y (q o r)'
+
+pr(0028-, ['p = V', and, 'q = V', and, 'r = V', 'q ^ (r v q)','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'q = V', and, 'r = F', 'q ^ (r v q)','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'q = F', and, 'r = V', 'q ^ (r v q)','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'q = F', and, 'r = F', 'q ^ (r v q)','='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'q = V', and, 'r = V', 'q ^ (r v q)','='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'q = V', and, 'r = F', 'q ^ (r v q)','='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'q = F', and, 'r = V', 'q ^ (r v q)','='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'q = F', and, 'r = F', 'q ^ (r v q)','='], ['F'], 20/20, ['Ray Valero']).
+
+% la implicacion '[p^((p^r)->s) -> (r->s)]' es una tautología
+
+pr(0028-, ['p = V', and, 'r = F', and, 's = F', '[p ^ ((p ^ r) ->s) -> (r->s)]','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'r = F', and, 's = V', '[p ^ ((p ^ r) ->s) -> (r->s)]','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'r = V', and, 's = F', '[p ^ ((p ^ r) ->s) -> (r->s)]','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'r = V', and, 's = V', '[p ^ ((p ^ r) ->s) -> (r->s)]','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'r = F', and, 's = F', '[p ^ ((p ^ r) ->s) -> (r->s)]','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'r = F', and, 's = V', '[p ^ ((p ^ r) ->s) -> (r->s)]','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'r = V', and, 's = F', '[p ^ ((p ^ r) ->s) -> (r->s)]','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'r = V', and, 's = V', '[p ^ ((p ^ r) ->s) -> (r->s)]','='], ['V'], 20/20, ['Ray Valero']).
+
+% demostraremos que las implicaciones '(p ^ q) -> r' y 'p -> (q->r)' son logicamente equivalentes
+% tabla de 'p -> (q->r)'
+
+pr(0028-, ['p = F', and, 'q = F', and, 'r = F', 'p -> (q->r)','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'q = F', and, 'r = V', 'p -> (q->r)','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'q = V', and, 'r = F', 'p -> (q->r)','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'q = V', and, 'r = V', 'p -> (q->r)','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'q = F', and, 'r = F', 'p -> (q->r)','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'q = F', and, 'r = V', 'p -> (q->r)','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'q = V', and, 'r = F', 'p -> (q->r)','='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'q = V', and, 'r = V', 'p -> (q->r)','='], ['V'], 20/20, ['Ray Valero']).
+
+% tabla de '(p ^ q) -> r'
+
+pr(0028-, ['p = F', and, 'q = F', and, 'r = F', '(p ^ q) -> r','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'q = F', and, 'r = V', '(p ^ q) -> r','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'q = V', and, 'r = F', '(p ^ q) -> r','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = F', and, 'q = V', and, 'r = V', '(p ^ q) -> r','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'q = F', and, 'r = F', '(p ^ q) -> r','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'q = F', and, 'r = V', '(p ^ q) -> r','='], ['V'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'q = V', and, 'r = F', '(p ^ q) -> r','='], ['F'], 20/20, ['Ray Valero']).
+pr(0028-, ['p = V', and, 'q = V', and, 'r = V', '(p ^ q) -> r','='], ['V'], 20/20, ['Ray Valero']).
+
+% Leyes de Morggan
+
+pr(0028-, [ 'Ley de De Morgan:'], ['~(p^q) = (~p)v(~q)'], 20/20, ['Ray Valero']).
+pr(0028-, [ 'Ley de De Morgan:'], ['~(pvq) = (~p)^(~q)'], 20/20, ['Ray Valero']).
